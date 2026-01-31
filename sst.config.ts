ee79e0c409d8f838fbdda7ddcf49e2aed576b166
yungblud <killi8n@gmail.com>
2026-01-31 11:52:44 +0900

    [feat] update sst config

--- sst.config.ts
+++ sst.config.ts
@@ -1,13 +1,17 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-unsafe-member-access */
+/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 // eslint-disable-next-line @typescript-eslint/triple-slash-reference
 /// <reference path="./.sst/platform/config.d.ts" />
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-require-imports
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 const dotenv = require('dotenv');
 
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access
 dotenv.config();
 
 export default $config({
+  // @ts-expect-error
   app(input) {
     return {
       name: 'aws-nestjs-container',
@@ -22,11 +26,15 @@ export default $config({
       },
     };
   },
+  // eslint-disable-next-line @typescript-eslint/require-await
   async run() {
     const vpc = new sst.aws.Vpc(process.env.AWS_VPC);
     const cluster = new sst.aws.Cluster(process.env.AWS_CLUSTER, { vpc });
     new sst.aws.Service(process.env.AWS_SERVICE, {
       cluster,
+      loadBalancer: {
+        ports: [{ listen: '80/http', forward: '3000/http' }],
+      },
       dev: {
         command: 'pnpm run start:dev',
       },
